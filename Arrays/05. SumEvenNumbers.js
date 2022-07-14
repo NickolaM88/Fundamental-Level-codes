3fdf3b31b88c0810be245a61c94d550c5d08b2d9
Nikola Marinov <102319114+NickolaM88@users.noreply.github.com>
2022-07-14 14:10:31 +0300

    Add files via upload

---
+++ Arrays/05. SumEvenNumbers.js
@@ -0,0 +1,13 @@
+function sumEven (arr){
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++){
+        arr[i] = Number(arr[i]);
+    }
+        for (const num of arr) {
+            if(num % 2 === 0){
+                sum += num;
+            }
+        }
+        console.log(sum);
+}
+sumEven([1,2,3,4,5,6])
