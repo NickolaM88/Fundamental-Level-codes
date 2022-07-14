0acca4fc0db44c28b1ab516a881fbe18dfe7bdc1
Nikola Marinov <102319114+NickolaM88@users.noreply.github.com>
2022-07-14 14:09:29 +0300

    Add files via upload

---
+++ Arrays/03. ReverseAnArrayOfNumbers.js
@@ -0,0 +1,16 @@
+function reversedArray(n, arr) {
+
+    let newArr = [];
+    for (let i = 0; i < n; i++) {
+        newArr.push(arr[i])
+    }
+
+    let result = '';
+    for (let i = newArr.length - 1; i >= 0; i--) {
+        result += newArr[i];
+        result += ' ';
+    }
+    console.log(result);
+}
+
+reversedArray([10, 20, 30, 40, 50])
