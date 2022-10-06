166baeb8efde493ad1f1656c994b3c679988ac5c
Nikola Marinov <102319114+NickolaM88@users.noreply.github.com>
2022-10-06 23:03:51 +0300

    Add files via upload

---
+++ Associative Arrays/01. Phone Book.js
@@ -0,0 +1,18 @@
+function phonebook(input) {
+    let result = {};
+    for (const line of input) {
+        let tokens = line.split(` `);
+        let name = tokens[0];
+        let phone = tokens[1];
+        result[name] = phone;
+    }
+    for (const name in result) {
+       console.log(`${name} -> ${result[name]}`);
+       
+    }
+}
+
+phonebook(["Tim 0834212554",
+"Peter 0877547887",
+"Bill 0896543112",
+"Tim 0876566344"])
