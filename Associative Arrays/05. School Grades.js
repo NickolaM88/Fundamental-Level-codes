c316ddb61aa617f9690266982c7ddc9ece576368
Nikola Marinov <102319114+NickolaM88@users.noreply.github.com>
2022-10-08 01:52:43 +0300

    Add files via upload

---
+++ Associative Arrays/05. School Grades.js
@@ -0,0 +1,35 @@
+function schoolGrades(input) {
+    let result = new Map();
+
+    for (let line of input) {
+        let tokens = line.split(` `);
+        let name = tokens.shift();
+        let grades = tokens.map(Number);
+        // ensure grades array always exist
+        if(result.has(name) == false){
+            result.set(name, []);
+        }
+        // finding existing grades and add new grades
+        let existing = result.get(name);
+        for (const grade of grades) {
+            existing.push(grade);
+        }
+    }
+    // sort by student name
+    let sorted = Array.from(result);
+    sorted.sort((a, b) => a[0].localeCompare(b[0]));
+
+    for (let [name, grades] of sorted) {
+        let average = 0;
+        for (let grade of grades) {
+            average += grade
+        }
+        average /= grades.length;
+        console.log(`${name}: ${average.toFixed(2)}`);
+    }
+}
+
+schoolGrades(["Lilly 4 6 6 5",
+    "Tim 5 6",
+    "Tammy 2 4 3",
+    "Tim 6 6"])
