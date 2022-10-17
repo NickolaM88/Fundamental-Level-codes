f5d122ae1bf5ae17b4dee80a9a05619a0707d8f8
Nikola Marinov <102319114+NickolaM88@users.noreply.github.com>
2022-10-17 23:52:02 +0300

    Add files via upload

---
+++ Data Types and Variables/01. SumDigits.js
@@ -0,0 +1,9 @@
+function sumDigits (num){
+    num = num.toString()
+    let sum = 0;
+    for(let i = 0;i < num.length ; i++){
+        sum += Number(num[i]);
+    }
+    console.log(sum);
+}
+sumDigits(231345)
