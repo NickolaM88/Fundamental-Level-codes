8f2b47671c45d7232fa8a719075bb8fe18d7b1df
Nikola Marinov <102319114+NickolaM88@users.noreply.github.com>
2022-11-14 17:29:54 +0200

    Add files via upload

---
+++ Object and Classes/01. Employees.js
@@ -0,0 +1,15 @@
+function solve (input){
+    let listOfEmployees = {};
+    for (let employee of input) {
+        listOfEmployees.name = employee;
+        listOfEmployees.personalNumber = employee.length;
+        console.log(`Name: ${listOfEmployees.name} -- Personal Number: ${listOfEmployees.personalNumber}`);
+    }
+}
+
+solve([
+    `Silas Butler`,
+    `Adnaan Buckley`,
+    `Juan Peterson`,
+    `Brendan Villarreal`
+    ])
