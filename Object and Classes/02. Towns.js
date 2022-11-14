3bead9df18b81ac846e356d24e8e82abcd7bac03
Nikola Marinov <102319114+NickolaM88@users.noreply.github.com>
2022-11-14 17:30:07 +0200

    Add files via upload

---
+++ Object and Classes/02. Towns.js
@@ -0,0 +1,14 @@
+function towns(input) {
+    let finalResult = {};
+    for (let tokens of input) {
+        let info = tokens.split(" | ");
+        let townName = info[0];
+        let latitude = Number(info[1]).toFixed(2);
+        let longitude = Number(info[2]).toFixed(2);
+
+        finalResult.town = townName;
+        finalResult.latitude = latitude;
+        finalResult.longitude = longitude;
+        console.log(finalResult);
+    }
+}
