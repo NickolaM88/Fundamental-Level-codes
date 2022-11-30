d6b9f016247a84c844da57b581ec2150b0864ce3
Nikola Marinov <102319114+NickolaM88@users.noreply.github.com>
2022-11-30 10:01:17 +0200

    Add files via upload

---
+++ Text Processing/06. Pascal-Case Splitter.js
@@ -0,0 +1,17 @@
+function solve(text) {
+    let words = [];
+    let currWord = text[0];
+
+    for (let index = 1; index < text.length; index++) {
+        if (text[index].toUpperCase() !== text[index]) {
+            currWord = currWord.concat(text[index]); // Sp
+        } else {
+            words.push(currWord);
+            currWord = text[index];
+        }
+    }
+    words.push(currWord);
+    console.log(words.join(`, `));
+}
+
+solve("SplitMeIfYouCanHaHaYouCantOrYouCan")
